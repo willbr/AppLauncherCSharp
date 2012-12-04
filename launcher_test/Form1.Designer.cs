220b6aeea10566338b7575d74057ecd639114d0f
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-12-04 21:22:43 +0000

    add; basic gui

---
+++ launcher_test/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace launcher_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(600, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(0, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(600, 190);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(0, 259);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(600, 292);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 563);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ launcher_test/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace launcher_test
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_HOTKEY = 0x0312;
+        private const int HOTKEY_ID = 0;
+
+        [Flags]
+        private enum MOD : uint
+        {
+            MOD_ALT = 0x01,
+            MOD_CONTROL = 0x02,
+            MOD_SHIFT = 0x04,
+            MOD_WIN = 0x08
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool RegisterHotKey(
+            IntPtr hWnd,
+            int id,
+            MOD fsModifiers,
+            uint vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        protected override void WndProc(
+            ref Message m
+            )
+        {
+            if (m.Msg == WM_HOTKEY) {
+                int id = m.WParam.ToInt32();
+                //MessageBox.Show(string.Format("Hotkey {0}", id));
+                this.Show();
+            }
+            base.WndProc(ref m);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(
+            object sender,
+            EventArgs e
+            )
+        {
+            this.Visible = false;
+            Log("load");
+            bool r = RegisterHotKey(this.Handle, HOTKEY_ID, MOD.MOD_CONTROL, (int)' ');
+            Log(string.Format("reg hk {0}", r));
+            if (r == false)
+            {
+                MessageBox.Show("failed to register hotkey");
+                this.Close();
+            }
+            listBox1.Items.Add("hello");
+            listBox1.Items.Add("two");
+            listBox1.Items.Add("see");
+        }
+
+        private void Log(
+            string message
+            )
+        {
+            textBox2.Text += message + Environment.NewLine;
+            textBox2.SelectionStart = textBox2.Text.Length;
+            textBox2.ScrollToCaret();
+        }
+
+        private void textBox1_TextChanged(
+            object sender,
+            EventArgs e
+            )
+        {
+            Log("change");
+        }
+
+        private void Form1_KeyUp(
+            object sender,
+            KeyEventArgs e
+            )
+        {
+            Log("up");
+
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    Log("escape");
+                    Close();
+                    //this.Hide();
+                    break;
+                case Keys.Enter:
+                    Log("enter");
+                    Log((string)listBox1.SelectedItem);
+                    listBox1.Items.Clear();
+                    break;
+                case Keys.Space:
+                    Log("space");
+                    break;
+                default:
+                    break;
+            }
+
+            if (e.Control)
+            {
+                Log("control");
+            }
+            else
+            {
+                ;
+            }
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            Log("deactivate");
+            this.Hide();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //MessageBox.Show("closed");
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+    }
+}
